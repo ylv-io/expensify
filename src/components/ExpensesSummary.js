5e3b1723e97cf75d2ae712406ce37aad66c8cdf3
Igor Yalovoy <igor@yalovoi.net>
2018-11-29 12:15:35 +0300

    ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,18 +7,23 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = (props) => (
-  <div>
-    <p>
-      Viewing {props.expenses.length} expenses totalling {numeral(props.expensesTotal / 100).format('$0,0.00')}
-    </p>
-  </div>
-);
+export const ExpensesSummary = ({ expensesTotal, expensesCount}) => {
+  const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
+  const formattedValue = numeral(expensesTotal / 100).format('$0,0.00');
+
+  return (
+    <div>
+      <p>
+        Viewing {expensesCount} {expenseWord} totalling {formattedValue}
+      </p>
+    </div>
+  );
+}
 
 const mapStateToProps = (state) => {
   const expenses = selectExpenses(state.expenses, state.filters);
   return {
-    expenses: expenses,
+    expensesCount: expenses.length,
     expensesTotal: expensesTotal(expenses),
   };
 };
